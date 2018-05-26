0026cc47e76bd43528835a2757dfe5714c518896
Kelemen Márton <kelemenmarci95@hotmail.com>
2018-05-26 15:34:42 +0200

    update firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -74,3 +74,12 @@ export const startRemoveExpense = ({ id } = {}) => {
       })
   }
 };
+
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).update(updates)
+      .then(() => {
+        dispatch(editExpense(id, updates))
+      })
+  };
+};
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, startRemoveExpense } from "../actions/expenses";
+import { startEditExpense, startRemoveExpense } from "../actions/expenses";
 
 export class EditExpensePage extends React.Component {
   onSubmit = expense => {
-    this.props.editExpense(this.props.expense.id, expense);
+    this.props.startEditExpense(this.props.expense.id, expense);
     this.props.history.push('/');
   };
 
@@ -33,7 +33,7 @@ const mapStateToProps = (state, props) => {
 
 const mapDispatchToProps = (dispacth, props) => {
   return {
-    editExpense: (id, expense) => dispacth(editExpense(id, expense)),
+    startEditExpense: (id, expense) => dispacth(startEditExpense(id, expense)),
     startRemoveExpense: (data) => dispacth(startRemoveExpense(data))
   }
 };
